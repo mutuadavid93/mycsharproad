1f0180668cf5dd34f28331c82a7fe6f182a5121c
Street Money <Street Money>
2016-09-27 20:40:11 +0300

    Finished C# properties solution

---
+++ CSharpProperties/CSharpProperties/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProperties
+{
+
+    /***************************
+     * 
+     *   C# PROPERTIES
+     * 
+     * *************************/
+
+    //Read / Write (get/set), Read Only(get), Write only(set)
+    //Auto implement property for fields requiring no logic
+
+    public class Student
+    {
+        private string _name;
+        private int _passmark = 39;
+
+        //Read/Write
+        public string Name{
+            set {
+                if(string.IsNullOrEmpty(value)) {
+                    throw new Exception("Name cannot be null nor empty");
+                }
+                else { this._name = value; }
+            }
+            get {
+                if(string.IsNullOrEmpty(this._name)) {
+                    return "No name";
+                }
+                else { return this._name; }
+            }
+        }
+
+        //Read only
+        public int Passmark{
+            get {
+                return this._passmark;
+            }
+        }
+
+        //Autoimplement property
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate and Invoke
+            Student std = new Student();
+            std.Name = "Derek Mkaba";
+            
+            //Set an autoimplemented prop
+            std.City = "Nairobi";
+
+            //print the name and city
+            Console.WriteLine("Student name: {0}, Passmark: {1} && City: {2}", std.Name, std.Passmark, std.City);
+        }
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+
+    public class Student
+    {
+        private string name;
+        private int _id;
+        private int passmark = 40;
+
+        //Use setter and getter methods to have control on
+        //what is assigned to the properties
+        //after using private access modifier
+
+        public void SetId(int id)
+        {
+
+            if (id <= 0)
+            {
+                throw new Exception("Student ID cannot be Negative");
+            }
+            else { this._id = id; }            
+        }
+
+        public int GetId()
+        {
+            return this._id;
+        }
+
+
+        //set the name
+        public void SetName(string Names)
+        {
+            if (string.IsNullOrEmpty(Names))
+            {
+                throw new Exception("Name cannot be null nor empty");
+            }
+            this.name = Names;
+        }
+
+        //get the name
+        public string GetName()
+        {
+            return string.IsNullOrEmpty(this.name) ? "No name" : this.name;
+        }
+
+        //Make the passmark to be Read Only
+        public int GetPassmark()
+        {
+            return this.passmark;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Invoke
+            Student std = new Student();
+            std.SetId(10);
+
+            std.SetName("Makali");
+
+            //Print the id
+            Console.WriteLine("STudent ID = {0} ", std.GetId());
+
+            //print Name
+            Console.WriteLine("STudent Name = {0} ", std.GetName());
+
+            //print the passmark
+            Console.WriteLine("Passmark = {0} ", std.GetPassmark());
+        }
+    }
+}
